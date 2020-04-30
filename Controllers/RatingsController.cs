3f34db176091facd0d4b723c4ddb94cf340902e1
John Lange <lange.john.m@gmail.com>
2020-04-30 10:26:10 -0400

    File upload is now supported

--- Controllers/RatingsController.cs
+++ Controllers/RatingsController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using RateYourDerp.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace RateYourDerp.Controllers
 {
@@ -28,15 +30,25 @@ namespace RateYourDerp.Controllers
         }
 
         // Save from Add page
-        public IActionResult Save(string Title, string Image, int Rating)
+        public IActionResult Save(string Title, IFormFile Image, int Rating)
         {
             Derp newderp = new Derp();
             newderp.Title = Title;
-            newderp.Image = Image;
+            newderp.Image = Image.FileName;
             newderp.Rating = Rating;
 
             db.Derps.Add(newderp);
-            db.SaveChanges();
+            db.SaveChanges(); // saves all of the database stuff
+
+            // save image on harddrive too
+            // open up a new file
+            FileStream fs = new FileStream("wwwroot/images/" + Image.FileName, FileMode.OpenOrCreate);
+            // dump in all of the image data
+            Image.CopyTo(fs);
+            // close/save file
+            fs.Dispose();
+
+
             return Redirect("/");
         }
     }
